a607c210fd4f9b9a7ec902b97afc94f9b82242b9
Seth B <seth.bennett.photo@gmail.com>
2026-07-07 20:21:28 -0700

    Fix Amplify sandbox assembly and generate outputs

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,6 @@
 import { defineBackend } from "@aws-amplify/backend";
-import { auth } from "./auth/resource";
-import { data } from "./data/resource";
+import { auth } from "./auth/resource.ts";
+import { data } from "./data/resource.ts";
 
 defineBackend({
   auth,
